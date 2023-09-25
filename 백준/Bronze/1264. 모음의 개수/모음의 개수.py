ce2f4939635449258a189e1881869937fe0dce48
ddioni <108507011+ddioni@users.noreply.github.com>
2023-09-25 15:53:38 +0900

    [Bronze IV] Title: 모음의 개수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -0,0 +1,11 @@
+while True:
+    s = input().lower()
+    if s == "#":
+        break
+
+    count = 0
+    for i in s:
+        if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+            count = count + 1
+
+    print(count)
